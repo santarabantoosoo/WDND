549fce020c51c201b652a9a06c2231c263368c1f
santarabantoosoo <31887357+santarabantoosoo@users.noreply.github.com>
2021-01-23 16:02:01 +0200

    task

---
+++ session3/session3-tasks/tasks/task_pretty printed Flask-sqlalchemy course/flask_sqlalchemy_basics/app.py
@@ -0,0 +1,154 @@
+# TODO 
+
+# add relationships to tables
+# apply migrations 
+# complete functions at the end of the file, then check them in flask shell
+
+
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
+from datetime import datetime, timedelta
+from faker import Faker 
+
+
+import random
+
+fake = Faker()
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+db = SQLAlchemy(app)
+#db.init_app(app)
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(500), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    postcode = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    order_date = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    shipped_date = db.Column(db.DateTime)
+    delivered_date = db.Column(db.DateTime)
+    coupon_code = db.Column(db.String(50))
+    customer_id = db.Column(db.Integer, db.ForeignKey('customer.id'), nullable=False)
+
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False)
+    price = db.Column(db.Integer, nullable=False)
+
+def add_customers():
+    for _ in range(100):
+        customer = Customer(
+            first_name=fake.first_name(),
+            last_name=fake.last_name(),
+            address=fake.street_address(),
+            city=fake.city(),
+            postcode=fake.postcode(),
+            email=fake.email()
+        )
+        db.session.add(customer)
+    db.session.commit()
+
+def add_orders():
+    customers = Customer.query.all()
+
+    for _ in range(1000):
+        #choose a random customer
+        customer = random.choice(customers)
+
+        ordered_date = fake.date_time_this_year()
+        shipped_date = random.choices([None, fake.date_time_between(start_date=ordered_date)], [10, 90])[0]
+
+        #choose either random None or random date for delivered and shipped
+        delivered_date = None
+        if shipped_date:
+            delivered_date = random.choices([None, fake.date_time_between(start_date=shipped_date)], [50, 50])[0]
+
+        #choose either random None or one of three coupon codes
+        coupon_code = random.choices([None, '50OFF', 'FREESHIPPING', 'BUYONEGETONE'], [80, 5, 5, 5])[0]
+
+        order = Order(
+            customer_id=customer.id,
+            order_date=ordered_date,
+            shipped_date=shipped_date,
+            delivered_date=delivered_date,
+            coupon_code=coupon_code
+        )
+
+        db.session.add(order)
+    db.session.commit()
+
+def add_products():
+    for _ in range(10):
+        product = Product(
+            name=fake.color_name(),
+            price=random.randint(10,100)
+        )
+        db.session.add(product)
+    db.session.commit()
+    
+def add_order_products():
+    orders = Order.query.all()
+    products = Product.query.all()
+
+    for order in orders:
+        #select random k
+        k = random.randint(1, 3)
+        # select random products
+        purchased_products = random.sample(products, k)
+        order.products.extend(purchased_products)
+        
+    db.session.commit()
+
+def create_random_data():
+    # db.create_all()
+    add_customers()
+    add_orders()
+    add_products()
+    add_order_products()
+
+def get_orders_by(customer_id=1):
+    print('Get Orders by Customer')
+
+def get_pending_orders():
+    print('Pending Orders')
+   
+def how_many_customers():
+    print('How many customers?')
+    
+def orders_with_code():
+    print('Orders with coupon code')
+    
+
+def revenue_in_last_x_days(x_days=30):
+    print('Revenue past x days')
+    
+def average_fulfillment_time():
+    print('Average fulfillment time')
+    
+def get_customers_who_have_purchased_x_dollars(amount=500):
+    print('All customers who have purchased x dollars')
+    
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db,
+     'get_customers_who_have_purchased_x_dollars':
+     get_customers_who_have_purchased_x_dollars,
+     'average_fulfillment_time': average_fulfillment_time,
+     'revenue_in_last_x_days': revenue_in_last_x_days,
+     'orders_with_code' : orders_with_code,
+     'how_many_customers':how_many_customers,
+     'get_pending_orders': get_pending_orders,
+     'get_orders_by' : get_orders_by,
+     'create_random_data' : create_random_data}        
